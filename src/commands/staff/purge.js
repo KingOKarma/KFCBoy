a01df819037bc336dcf663ef82d02fcb794acfd9
KingOKarma <kainesimpkins782@gmail.com>
2020-09-13 01:49:41 +0100

    Finally added purge w/ staff module toggle

--- src/commands/staff/purge.js
+++ src/commands/staff/purge.js
@@ -1,55 +1,38 @@
+const Toggle = require("../../models/toggle.js");
+
 module.exports = {
     name: 'purge',
     aliases: ["prune", "delete"],
 
+    run: async (_, message, args) => {
+        Toggle.findOne({
+            ServerID: message.guild.id,
+            Command: "Staff"
+        },
+            (err, toggle) => {
 
-    run: async (_, message, prefix, args) => {
+                if (err) console.log(err);
+                if (!toggle) {
 
+                    if (!message.member.permissionsIn(message.channel).has("MANAGE_MESSAGES")) return message.channel.send("You need the permission __**\"Mange Messages\"**__ to use this command")
+                    if (!message.guild.me.permissionsIn(message.channel).has("MANAGE_MESSAGES")) return message.channel.send("I need the permission __**\"Mange Messages\"**__ to use this command")
 
-        message.reply("Sorry this command is still under constuction")
+                    const deleteCount = parseInt(args[0], 10);
+                    console.log(args[0])
 
-        // const argss = message.content.split(' ').slice(1);
-        // const amount = argss.join(' ');
+                    if (!deleteCount || deleteCount < 1 || deleteCount > 100)
+                        return message.reply("Please provide a number between 1 and 100 for the number of messages to delete");
 
+                    message.delete()
+                    setTimeout(async () => {
+                        const fetched = await message.channel.messages.fetch({ limit: deleteCount });
+                        message.channel.bulkDelete(fetched)
+                            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+                    }, 100);
+                }
+                if (toggle) return message.channel.send("This server has the \"Staff\" module disabled")
+            })
 
-        // async function purge() {
-        //     try {
-        //         await message.delete();
-        //     } catch (err) { }
-        //     if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-        //         return message.reply("❌ You do not have the **MANAGE MESSAGES** permission. Please contact a staff member")
-        //             .then(m => m.delete(20000));
-        //     }
-
-        //     // No bot permissions
-        //     if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-        //         return message.reply("❌ I do not have permissions to delete messages. Please contact a staff member")
-        //             .then(m => m.delete(20000));
-        //     }
-
-
-        //     if (isNaN(argss[0])) {
-        //         // Sends a message to the channel.
-        //         message.channel.send(`Please use a number as your arguments. \n \`Usage ${prefix}purge <amount>\``); //\n means new line.
-        //         // Cancels out of the script, so the rest doesn't run.
-        //         return;
-        //     }
-
-        //     if (!args[1] > 100) return message.reply('You can`t delete more than 100 messages at once!')
-
-        //     if (!args[1] > 1) return message.reply('You have to delete at least 1 message!')
-
-
-        //     await msg.channel.messages.fetch({ limit: amount }).then(messages => {
-        //         msg.channel.bulkDelete(messages
-
-        //         )
-        //     });
-
-
-        // }
-
-        // purge()
     }
 }
 
