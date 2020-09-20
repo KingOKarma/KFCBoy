1fd7fc2221fc6a61f7a5bfbdc6e5acd13c7cb7f5
KingOKarma <kainesimpkins782@gmail.com>
2020-09-20 22:07:45 +0100

    add git cmds

---
+++ src/commands/owner/commit.js
@@ -0,0 +1,63 @@
+
+
+module.exports = {
+    name: 'commit',
+    aliases: ["gitcommit"],
+
+    run: (_, message, args) => {
+
+        if (message.author.id != "406211463125008386") {
+            message.reply("Sorry This command can only be used by Kaine >:( this is just so you guys dont break anything!")
+            return
+        }
+
+
+        const { exec } = require("child_process");
+        
+
+        console.log('Starting directory: ' + process.cwd());
+        try {
+            process.chdir('../');
+            console.log('New directory: ' + process.cwd());
+          }
+          catch (err) {
+            console.log('chdir: ' + err);
+            return
+          }
+
+
+
+
+        exec(`git commit -m "${args.join(' ')}"`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                process.chdir('./src');
+                message.channel.send("I cant commit to the repo for some reason <:RoScreaming:719628209402019980>\n That reason being ```" + error + "```\nSo i moved back into ```" + process.cwd() + "```\nuse `rbstatus` To check for files!")
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            message.channel.send(`Git commit request: \n\`\`\`${stdout}\`\`\``)
+            message.channel.send(`\`\`\`Current directory: ${process.cwd()}\`\`\``);
+
+        })
+
+        console.log('Final directory: ' + process.cwd());
+        try {
+            process.chdir('./src');
+            console.log('New directory: ' + process.cwd());
+          }
+          catch (err) {
+            console.log('chdir: ' + err);
+            return
+          }
+
+
+
+
+
+    }
+}
---
+++ src/commands/owner/webhook.js
@@ -0,0 +1,69 @@
+const Discord = require("discord.js")
+module.exports = {
+    name: 'webhook',
+    run: async (_, message, args, bot, token, client) => {
+        if (message.author.id != "406211463125008386") return message.channel.send("This is a Owner only comamnd! <:Kaineshrug:711591140125704242>")
+
+        if (message.channel.fetchWebhooks()
+        
+        
+        ) {
+            message.channel.createWebhook("test");
+            message.channel.send("No Current Webhook found, Creating new one...");
+            console.log("Created Webhook")
+
+        }
+
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Some Title')
+            .setColor('#0099ff');
+
+
+        const webhooks = await message.channel.fetchWebhooks();
+        const webhook = webhooks.first();
+        await webhook.send('Webhook test', {
+            username: 'some-username',
+            avatarURL: 'https://i.imgur.com/wSTFkRM.png',
+            embeds: [embed],
+            
+        });
+        
+
+
+
+
+
+
+        // var request = new XMLHttpRequest();
+        // request.open("POST", "https://discordapp.com/api/webhooks/676118118082281513/ZS5YcWhurzokBrKX9NgexqtxrJA5Pu2Bo4i7_JsIxC-JIbPBVhSZkcVVukGOro52rnQA");
+
+        // request.setRequestHeader('Content-type', 'application/json');
+
+        // var params = {
+        //   username: "My Webhook Name",
+        //   avatar_url: "",
+        //   content: "The message to send"
+        // }
+
+
+
+
+
+
+        //   const config = require('../../config.json');
+
+        // const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+
+        // const embed = new Discord.MessageEmbed()
+        //     .setTitle('Some Title')
+        //     .setColor('#0099ff');
+
+        // webhookClient.send('Webhook test', {
+        //     username: 'some-username',
+        //     avatarURL: 'https://i.imgur.com/wSTFkRM.png',
+        //     embeds: [embed],
+        // });
+
+    }
+}
