6d2401793b659c2c0bbe22edbf37ceef794c8c81
BuildTools <unconfigured@null.spigotmc.org>
2020-11-14 22:44:35 +0100

    minor idiotic mistakes fixed :)

--- src/commands/Currency/work.js
+++ src/commands/Currency/work.js
@@ -4,27 +4,15 @@ const currency = require("../../models/currency");
 const stopWork = new Set();
 var utils = require("../../utils/smartUtils");
 
-/* WORK EMBED
-      const embed = new Discord.MessageEmbed()
-        .setAuthor(
-          message.author.tag,
-          message.author.displayAvatarURL
-        )
-        .setThumbnail(Thumbnail)
-        .addField(
-          "jobs list",
-          "Chicken Nuggie fryer\nChicken nuggie inspector\nChicken nuggie taste tester\n",
-          true
-        )
-        .addField(
-          "job nickname",
-          "Fryer\nInspector\ntaste-tester\n\n",
-          true
-        )
-        .setFooter(
-          '\nto "apply" for a job use k!work [Work nickname here]'
-        );
-*/
+var leaveMessages = [
+  "Your boss was happy that you left bc you didnt really do that much work",
+  "Your boss begged you to stay but you were to much of a savage to care",
+  "You have left your job even tho you didnt really have enough money to live life. So you start to think that you might need to become homeless to stay alive",
+  "Im happy that you left. You just produced wayyy to many nuggies for us to keep up with the sales",
+  "Wow you just left that place like it was nothing",
+  "You got so emotional while resigning from your job. that your boss wanted to give you a rise, but you went all cry mode and ran out of your boss' office and never came back",
+  "You told your boss that he was a dick and he fired you on the spot"
+];
 
 module.exports = {
   name: "work",
@@ -37,13 +25,11 @@ module.exports = {
       (err, toggle) => {
         if (err) console.log(err);
 
-
-        let guildicon = message.guild.iconURL({ dynamic: true })
-
-                    if (!message.guild.iconURL()) {
-                        guildicon = "https://cdn.discordapp.com/attachments/643347490925445132/758369629155360818/2Q.png"
-                    }
-
+        let guildicon = message.guild.iconURL({ dynamic: true });
+        if (!message.guild.iconURL()) {
+          guildicon =
+            "https://cdn.discordapp.com/attachments/643347490925445132/758369629155360818/2Q.png";
+        }
 
         if (!toggle) {
           currency.findOne(
@@ -81,10 +67,10 @@ module.exports = {
                   //}
                   user.Nuggies = user.Nuggies + earnings;
                   user.save().catch((err) => utils.logErr(message, err));
-                  message.channel.send(earnings);
+                  message.channel.send(`you earned ${earnings} <:chickennuggie:706268265424355399>`);
                 }
               } else {
-                switch (args[0]) {
+                switch (args[0].toLowerCase()) {
                   case "fryer":
                     if (!user) {
                       var newfryer = new currency({
@@ -95,17 +81,77 @@ module.exports = {
                       newfryer
                         .save()
                         .catch((err) => utils.logErr(message, err));
+                      message.channel.send("Your application as Fryer has been accepted. Welcome")
                     } else if (user.work != "") {
                       message.channel.send(
                         `As much as we would love to hire you as a fryer you already got a job at ${user.work}`
                       );
                     } else {
-                      user.work == "Fryer";
+                      user.work = "Fryer";
                       user.save().catch((err) => utils.logErr(message, err));
+                      message.channel.send("Your application as Fryer has been accepted. Welcome")
+                    }
+                    break;
+
+                  case "inspector":
+                    if (!user) {
+                      var newInspector= new currency({
+                        UserID: message.author.id,
+                        ServerID: message.guild.id,
+                        work: "inspector",
+                      });
+                      newInspector
+                        .save()
+                        .catch((err) => utils.logErr(message, err));
+                      message.channel.send("Your application as Inspector has been accepted. Welcome")
+                    } else if (user.work != "") {
+                      message.channel.send(
+                        `As much as we would love to hire you as a inspector you already got a job at ${user.work}`
+                      );
+                    } else {
+                      user.work = "Fryer";
+                      user.save().catch((err) => utils.logErr(message, err));
+                      message.channel.send("Your application as Inspector has been accepted. Welcome")
+                    }
+                    break;
+
+                  case "taste-tester":
+                    if (!user) {
+                      var newtaster = new currency({
+                        UserID: message.author.id,
+                        ServerID: message.guild.id,
+                        work: "taste-tester",
+                      });
+                      newtaster
+                        .save()
+                        .catch((err) => utils.logErr(message, err));
+                      message.channel.send("Your application as taste tester has been accepted. Welcome")
+                    } else if (user.work != "") {
+                      message.channel.send(
+                        `As much as we would love to hire you as a taste-tester you already got a job at ${user.work}`
+                      );
+                    } else {
+                      user.work = "taste-tester";
+                      user.save().catch((err) => utils.logErr(message, err));
+                      message.channel.send("Your application as taste tester has been accepted. Welcome")
+                    }
+                    break;
+                  case "help":
+                    break;
+                  case "leave":
+                    if (user.work == "") {
+                      message.channel.send("You dont have a job yet");
+                    } else {
+                      user.work = "";
+                      user.save().catch((err) => utils.logErr(err));
+                      message.channel.send(leaveMessages[Math.floor(Math.random() * leaveMessages.length)]);
                     }
                     break;
 
                   default:
+                    message.channel.send(
+                      "Invalid action use `k!work help` for more information"
+                    );
                     break;
                 }
               }
--- src/commands/owner/logs.js
+++ src/commands/owner/logs.js
@@ -1,49 +1,44 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
+
+const devs = ["406211463125008386", "355993074117115914"];
 module.exports = {
-    name: 'pm2',
-    aliases: ["pm2logs"],
-    run: async (_, message, args, bot, token, client) => {
-
-        if (message.author.id != "406211463125008386") {
-            message.reply("Sorry This command can only be used by Kaine >:( this is just so you guys dont break anything!")
-            return
-        }
-
-
-        const { exec } = require("child_process");
-        
-
-
-
-        exec(`pm2 logs 12 --nostream`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                message.channel.send("I cant check pm2 logs <a:VillagerCursedStare:719904898954821712>\n That reason being ```" + error + "```")
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                message.channel.send(`pm2 logs request: \n\`\`\`js\n${stderr}\`\`\``)
-
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            message.channel.send(`pm2 logs request: \n\`\`\`js\n${stdout}\`\`\``)
-
-        })
-
-
-        message.channel.send("logfile:", { files: [
-            "/home/karma/.pm2/logs/KFCBoy-out.log",
-            "/home/karma/.pm2/logs/KFCboy-error.log"
-        ] });
-
-
-
-
-
+  name: "pm2",
+  aliases: ["pm2logs"],
+  run: async (_, message, args, bot, token, client) => {
+    if (devs.some((dev) => dev != message.author.id)) {
+      message.reply(
+        "Sorry This command can only be used by Kaine >:( this is just so you guys dont break anything!"
+      );
+      return;
     }
 
+    const { exec } = require("child_process");
 
+    exec(`pm2 logs 12 --nostream`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        message.channel.send(
+          "I cant check pm2 logs <a:VillagerCursedStare:719904898954821712>\n That reason being ```" +
+            error +
+            "```"
+        );
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        message.channel.send(`pm2 logs request: \n\`\`\`js\n${stderr}\`\`\``);
 
-}
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      message.channel.send(`pm2 logs request: \n\`\`\`js\n${stdout}\`\`\``);
+    });
+
+    message.channel.send("logfile:", {
+      files: [
+        "/home/karma/.pm2/logs/KFCBoy-out.log",
+        "/home/karma/.pm2/logs/KFCboy-error.log",
+      ],
+    });
+  },
+};
