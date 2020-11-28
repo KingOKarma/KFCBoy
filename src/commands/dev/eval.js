918105630ab38e3427b92dd44a17d359bb2f7367
milas <59763532+milaskiller@users.noreply.github.com>
2020-11-28 22:51:05 +0100

    Update eval.js

---
+++ src/commands/dev/eval.js
@@ -0,0 +1,45 @@
+const devs = ["406211463125008386", "355993074117115914"];
+
+
+module.exports = {
+  name: "eval",
+  aliases: [],
+  run: (_, message, args) {
+   if (!devs.some((dev) => dev == message.author.id))
+      return message.channel.send(
+        "This is a Owner only comamnd! <:Kaineshrug:711591140125704242>"
+      );
+  
+  try {
+      var code = args.join(" ")
+      code.replace("(process.env.DISCORD_BOT_TOKEN)", "token")
+      let evaled = await require("util").inspect(eval(code));
+      if(typeof evaled != String) evaled = require("util").inspect(evaled);
+      evaled.replace(`${process.env.DISCORD_BOT_TOKEN}`, "TOKEN");
+      if(evaled.length > 1024) {
+        return message.channel.send("text too long ;)")
+      } else {
+        message.channel.send({
+          embed: {
+            title: message.author.username + " evaled",
+            fields: [
+              { name: `Input`, value: `\`\`\`js\n${args.join(' ')}\`\`\`` },
+              { name: `Output`, value: `\`\`\`js\n${evaled}\`\`\`` }
+            ]
+          }
+        })
+        console.log(message.author.username + " executed code:" + args.join(' '))
+      }
+    } catch (err) {
+      message.channel.send({
+        embed: {
+          title: message.author.username + " evaled",
+          fields: [
+            { name: `Input`, value: `\`\`\`js\n${args.join(' ')}\`\`\`` },
+            { name: `Output`, value: `\`\`\`js\n${err}\`\`\`` }
+          ]
+        }
+      })
+    }
+  }
+}
